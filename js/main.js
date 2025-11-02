62c88f654f1ae68a43fdebedfd785e5e6dbc8c05
Ruevel Ziegé <ruevelziege01@gmail.com>
2025-11-02 23:23:29 +0900

    Update fitur login dan tampilan utama

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('HAPPY ANIVERSARY RANIKU').split('')
+    const titles = ('HAPPY ANNIVERSARY RANIKU').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
