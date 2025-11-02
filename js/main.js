46a2ba89f01e29363c4714cd0fc2c90bc9eef4ef
Ruevel Ziegé <ruevelziege01@gmail.com>
2025-11-02 23:28:12 +0900

    Update tampilan menu utama

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('HAPPY ANNIVERSARY RANIKU').split('')
+    const titles = ('HAPPY ANNIVERSARY RANIKU SAYANGKU').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
